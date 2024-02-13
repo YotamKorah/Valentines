abaec849f65c0bc3ef004d2811e0ccc52e5b6e2b
Yotam Korah <yotamkorah@gmail.com>
2024-02-14 00:19:14 +0200

    eslint

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10 lg:p-24">
       <h1 className="text-6xl font-bold text-center">
-        Happy Valentine's Day!
+        Happy Valentine&apos;s Day!
       </h1>
       <FavoriteIcon sx={{ fontSize: 150 }} />
       <h3 className="text-2xl font-bold text-center mb-5">
-        "Sometimes all you need is a hug from the right person and all your stress will melt away."
+        &quot;Sometimes all you need is a hug from the right person and all your stress will melt away.&quot;
       </h3>
       <div className="flex justify-center flex-col lg:flex-row">
         <Image
